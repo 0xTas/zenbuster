cd4a60bdf5da6b948694f4df4a91cbeb044a264c
BuildTools <unconfigured@null.spigotmc.org>
2022-05-25 19:40:24 -0700

    copyright/license notice

--- zenbuster.py
+++ zenbuster.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 
-##################################################
-#                 ZenBuster.py                   #
-#  Multi-Platform Multithreaded URL Enumeration  #
-#       Author: Zach Griffin, aka: 0xTas         #
-#            Email: admin@0xTas.dev              #
-#           https://github.com/0xTas             #
-##################################################
+             ##################################################
+             #                 ☯ZenBuster.py                 #
+             #  Multi-Platform Multithreaded URL Enumeration  #
+             #       Author: Zach Griffin, aka: 0xTas         #
+             #            Email: admin@0xTas.dev              #
+             #           https://github.com/0xTas             #
+             ##################################################
+
+##############################################################################
+#                      © Copyright 2022 Zach Griffin                         #
+#                                                                            #
+#    This program is free software: you can redistribute it and/or modify    #
+#    it under the terms of the GNU General Public License as published by    #
+#    the Free Software Foundation, either version 3 of the License, or       #
+#    (at your option) any later version.                                     #
+#                                                                            #
+#    This program is distributed in the hope that it will be useful, but     #
+#    WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    #
+#    See the GNU General Public License for more details.                    #
+#                                                                            #
+#    You should have received a copy of the GNU General Public License       #
+#    along with this program. If not, see <https://www.gnu.org/licenses/>    #
+##############################################################################
 
 __author__ = 0x546173
 banner = """ 
